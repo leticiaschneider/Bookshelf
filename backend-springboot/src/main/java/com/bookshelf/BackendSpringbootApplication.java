8d4249f00cb2620ff931b9ec457db9f1a937f79c
leticiaschneider <leticia.schneider@gmail.com>
2024-06-10 19:00:00 -0300

    fix conflict

--- backend-springboot/src/main/java/com/bookshelf/BackendSpringbootApplication.java
+++ backend-springboot/src/main/java/com/bookshelf/BackendSpringbootApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendSpringbootApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootApplication.class, args);
 	}
+
 }
