0bde0cea683491b2d55a6d02f6609e034493cb8d
leticiaschneider <leticia.schneider@gmail.com>
2024-06-15 20:49:47 -0300

    add POST endpoint for creating book

--- backend-springboot/src/main/java/com/bookshelf/controllers/BookController.java
+++ backend-springboot/src/main/java/com/bookshelf/controllers/BookController.java
@@ -1,11 +1,14 @@
 package com.bookshelf.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bookshelf.domain.Book;
 import com.bookshelf.services.BookService;
@@ -21,5 +24,16 @@ public class BookController {
 	public ResponseEntity<Book> findById(@PathVariable Integer id) {
 		Book obj = service.findById(id);
 		return ResponseEntity.ok().body (obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Book>> findAll() {
+	    List<Book> list = service.findAll();
+	    return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public Book createEmployee(@RequestBody Book book) {
+		return service.createBook(book);
 	}
 }
--- backend-springboot/src/main/java/com/bookshelf/services/BookService.java
+++ backend-springboot/src/main/java/com/bookshelf/services/BookService.java
@@ -2,8 +2,11 @@ package com.bookshelf.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bookshelf.domain.Book;
 import com.bookshelf.repositories.BookRepository;
@@ -22,6 +25,11 @@ public class BookService {
 
 	public List<Book> findAll() {
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public Book createBook(@RequestBody Book book) {
+		return repository.save(book);
 	}
 
 }
