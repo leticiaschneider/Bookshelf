337dbb5769f91ed76ac7fd48ece01f1822de7d95
leticiaschneider <leticia.schneider@gmail.com>
2024-06-18 19:55:23 -0300

    add logo and designed list css using boostrasp

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    // path: 'home', canActivate: [AuthGuardService], // search about it
-    path: 'home',
+    path: '',
     loadChildren: () => import('./books/books.module').then((m) => m.BooksModule),
   },
 ];
