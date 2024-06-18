6eeae3fd1a8e42c1f9f5682e1e557d893213dd9a
leticiaschneider <leticia.schneider@gmail.com>
2024-06-18 19:26:55 -0300

    add routing for book list, create, and edit forms

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // path: 'home', canActivate: [AuthGuardService], // search about it
+    path: 'home',
+    loadChildren: () => import('./books/books.module').then((m) => m.BooksModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
