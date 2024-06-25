e9d2a781d5083b552b8ad7de9b979d21fc11a64b
leticiaschneider <leticia.schneider@gmail.com>
2024-06-25 19:16:34 -0300

    service for save book

--- frontend-angular/src/app/books/components/book-form/book-form.component.ts
+++ frontend-angular/src/app/books/components/book-form/book-form.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Book, StatusReading, Format } from '../../../model/book.model';
+import { Subject } from 'rxjs/internal/Subject';
+import { BookService } from '../../../service/book.service';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.scss'
 })
-export class BookFormComponent {
+export class BookFormComponent implements OnDestroy {
   bookForm!: FormGroup;
   readingStatuses = Object.values(StatusReading);
   formats = Object.values(Format);
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private bookService: BookService) {}
 
   ngOnInit(): void {
     this.bookForm = this.fb.group({
@@ -22,14 +26,66 @@ export class BookFormComponent {
       language: [''],
       pages: [null],
       genre: [''],
-      coverImageUrl: [''],
-      readingStatus: [[]],
-      formats: [[]]
+      coverImageUrl: [null],
+      readingStatus: [null],
+      formats: [null]
     });
   }
 
   onSubmit(): void {
     const book: Book = this.bookForm.value;
     console.log(book);
+
+    if (this.bookForm.valid) {
+      const bookData = this.bookForm.value;
+      bookData.readingStatus = [this.mapReadingStatusToCode(bookData.readingStatus)];
+      bookData.formats = [this.mapFormatToCode(bookData.formats)];
+
+      this.bookService.saveBook(bookData)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(
+          (savedBook) => {
+            console.log('Book saved successfully:', savedBook);
+          },
+          (error) => {
+            console.error('Error saving the book:', error);
+          }
+        );
+    } else {
+      console.error('Invalid form. Please check the fields.');
+    }
+  }
+
+  mapReadingStatusToCode(status: StatusReading): number {
+    switch (status) {
+      case StatusReading.Read:
+        return 0;
+      case StatusReading.Reading:
+        return 1;
+      case StatusReading.WantToRead:
+        return 2;
+      default:
+        throw new Error('Invalid reading status');
+    }
+  }
+
+  mapFormatToCode(format: Format): number {
+    switch (format) {
+      case Format.Hardcover:
+        return 0;
+      case Format.Paperback:
+        return 1;
+      case Format.eBook:
+        return 2;
+      case Format.Audiobook:
+        return 3;
+      default:
+        throw new Error('Invalid format');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
--- frontend-angular/src/app/books/components/book-list/book-list.component.ts
+++ frontend-angular/src/app/books/components/book-list/book-list.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Book } from '../../../model/book.model';
 import { BookService } from '../../../service/book.service';
+import { Subject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit, OnDestroy {
   books: Book[] = [];
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(private bookService: BookService) { }
 
@@ -21,5 +23,10 @@ export class BookListComponent {
         console.error('Error fetching books: ', error);
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
